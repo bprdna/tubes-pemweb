89c8ed8ab3c742dec38a0c48ee6d90cf0805a55c
Bintang Pradana <bintangpradana02@student.uns.ac.id>
2020-07-03 13:08:44 +0700

    basic layout

---
+++ resources/views/post/post.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card">
+                <img src="https://dummyimage.com/600x400/000/fff" alt="" class="card-img">
+                <div class="card-body">
+                    <h4 class="card-title">Judul</h4>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Aperiam temporibus neque dicta! Consequuntur voluptatum soluta incidunt iste a 
+                        maxime perspiciatis sit enim maiores molestiae sequi perferendis possimus deserunt, 
+                        magni exercitationem.
+                    </p>
+                </div>
+                <div class="card-footer text-muted">
+                    <a class="btn btn-primary" href="#">Daftar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
