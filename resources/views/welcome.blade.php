9a16ea24b3fd07fd4e8a4fb3cd8fa7973b3466cc
CandraTP <candratangguh@gmail.com>
2020-07-08 21:26:30 +0700

    seminar card update 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
             <img src="https://dummyimage.com/600x400/000/fff" alt="" class="card-img">
             <div class="card-body">
                 <h4 class="card-title">{{ $s->title }}</h4>
-                <p class="card-text">{{ $s->description }} .....</p>
+                <p class="card-text">{{ Str::limit($s->description, 80) }}</p>
                 <h5 class="card-text" style="font-size: 16px; margin-bottom: 5px">{{ $s->location }}</h5>
                 <h6 class="card-text" style="margin-bottom: 15px">{{ $s->date }}</h6>
                 <a class="btn btn-primary" href="#"">Beli Tiket</a>
