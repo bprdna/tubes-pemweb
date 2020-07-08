143dc198a9b0dbbdfea1ba7eba2e30583db8c06c
CandraTP <candratangguh@gmail.com>
2020-07-08 20:51:26 +0700

    seminar card update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,11 @@
             <img src="https://dummyimage.com/600x400/000/fff" alt="" class="card-img">
             <div class="card-body">
                 <h4 class="card-title">{{ $s->title }}</h4>
-                <p class="card-text">{{ $s->description }}</p>
-                <a class="btn btn-primary" href="#"">Button 1</a>
-                <a class="btn btn-danger" href="#"">Button 2</a>
+                <p class="card-text">{{ $s->description }} .....</p>
+                <h5 class="card-text" style="font-size: 16px; margin-bottom: 5px">{{ $s->location }}</h5>
+                <h6 class="card-text" style="margin-bottom: 15px">{{ $s->date }}</h6>
+                <a class="btn btn-primary" href="#"">Beli Tiket</a>
+                <a class="btn btn-danger" href="#"">Selengkapnya</a>
             </div>
         </div>
         @endforeach
