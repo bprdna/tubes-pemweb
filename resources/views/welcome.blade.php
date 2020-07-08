7da406610f8dec5b76296c73e5d7821feb2ca518
CandraTP <candratangguh@gmail.com>
2020-07-08 22:02:29 +0700

    update card 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,10 @@
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">{{ $s->title }}</h4>
-                <p class="card-text">{{ Str::limit($s->description, 80) }}</p>
+                <p class="card-text">{{ Str::limit($s->description, 100) }}</p>
                 <h5 class="card-text" style="font-size: 16px; margin-bottom: 5px">{{ $s->location }}</h5>
                 <h6 class="card-text" style="margin-bottom: 15px">{{ $s->date }}</h6>
-                <a class="btn btn-primary" href="#"">Beli Tiket</a>
-                <a class="btn btn-danger" href="#"">Selengkapnya</a>
+                <a class="btn btn-primary" href="/home/daftar/{{ $s->id }}">Daftar</a>
             </div>
         </div>
         @endforeach
